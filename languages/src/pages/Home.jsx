a2a0997b4eb45dfe22e00aad9b162769d87302ee
spiderasasen <spiderasasen@gmail.com>
2026-02-03 14:49:05 -0500

    made the basic layout idea for my home page now to add python

--- languages/src/pages/Home.jsx
+++ languages/src/pages/Home.jsx
@@ -3,11 +3,12 @@ import {useState, useEffect} from 'react'
 function Home(){
     return(
         <div>
-            <h1>Please select a Language</h1>
+            <h1>Major Languages from Around the World</h1>
+            <h3>Please chose 1 of the 3 options</h3>
             <div>
-                <li>
-                    <p>hi</p>
-                </li>
+                <li>Search via Continent</li>
+                <li>Search via Category</li>
+                <li>Look at all Languages</li>
             </div>
         </div>
     );
