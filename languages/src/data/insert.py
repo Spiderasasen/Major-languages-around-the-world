6686205ad4f3959d28a76e80bd8313521715d0da
spiderasasen <spiderasasen@gmail.com>
2026-02-04 08:13:55 -0500

    makeing the path to load my credentials

---
+++ languages/src/data/insert.py
@@ -0,0 +1,14 @@
+import mysql.connector
+import json
+
+#path to load credentials
+def load_credentials(path = "credentials.txt"):
+    credentials = {}
+    with open(path, "r") as f:
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
